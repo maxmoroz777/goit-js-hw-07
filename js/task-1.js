22d50f6004e3cc5a1805573989581f33ef324625
Max Moroz <maksim.moroz@gmail.com>
2024-06-10 18:31:44 +0100

    up

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const itemsList = document.querySelectorAll(".item");
+console.log("Number of categories: " + itemsList.length);
+
+const headerList = document.querySelector("h2");
+console.log("Category: " + headerList.textContent);
+
+const elementsList = document.querySelectorAll("li");
+console.log("Elements: " + elementsList.length);
+console.log('hello');
